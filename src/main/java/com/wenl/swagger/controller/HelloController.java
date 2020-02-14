017429805b4e932a1dd593940304f475d123141c
Shinelon <a1255391360>
2020-02-14 18:42:10 +0800

    2-14 18：32

--- src/main/java/com/wenl/swagger/controller/HelloController.java
+++ src/main/java/com/wenl/swagger/controller/HelloController.java
@@ -3,11 +3,13 @@ package com.wenl.swagger.controller;
 import com.wenl.swagger.pojo.User;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api(description = "Hello控制类")
 @RestController
 public class HelloController {
 
@@ -21,5 +23,17 @@ public class HelloController {
     @PostMapping("/getUser")
     public User getUser(){
         return new User();
+    }
+
+    @ApiOperation("getUser2方法接口描述")
+    @PostMapping("/getUser2")
+    public User getUser2(@ApiParam("用户名") String username){
+        return new User();
+    }
+
+    @ApiOperation("getUser3Post方法接口描述")
+    @PostMapping("/getUser3")
+    public User getUser3(@ApiParam("用户") User user){
+        return user;
     }
 }
